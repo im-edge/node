244c91c7c5001b3a83ed1172816701d698b2ed22
Thomas Gelf <thomas@gelf.net>
2024-08-04 21:07:19 +0200

    cli: introduce the "listen" command

---
+++ src/Command/ListenCommand.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace IMEdge\Node\Command;
+
+use GetOpt\ArgumentException\Missing;
+use GetOpt\Command;
+use GetOpt\GetOpt;
+use GetOpt\Option;
+use IMEdge\Node\NodeRunner;
+use IMEdge\Node\Rpc\SimpleClient;
+use Psr\Log\LoggerAwareInterface;
+use Psr\Log\LoggerAwareTrait;
+use Revolt\EventLoop;
+
+class ListenCommand extends Command implements LoggerAwareInterface
+{
+    use LoggerAwareTrait;
+
+    public function __construct()
+    {
+        parent::__construct('listen', $this->handle(...));
+        $this->setDescription('Listen on a given TCP socket address');
+        $this->addOptions([
+            Option::create(null, 'socket', GetOpt::REQUIRED_ARGUMENT)
+                ->setDescription('Listening socket, e.g. --listen 0.0.0.0:5661'),
+            Option::create(null, 'persist')
+                ->setDescription('Persist listener, will be enabled at every start'),
+        ]);
+    }
+
+    public function handle(GetOpt $options): void
+    {
+        $socket = $options->getOption('socket');
+        if ($socket === null) {
+            throw new Missing("Option 'socket' is required");
+        }
+        $this->logger->debug('Listening on ' . $socket);
+        $rpc = new SimpleClient(NodeRunner::SOCKET_FILE, $this->logger);
+        try {
+            if ($rpc->request('node.listen', [$socket, (bool) $options->getOption('persist')])) {
+                $this->logger->notice("Listening on $socket");
+            }
+            EventLoop::queue(fn () => exit(0));
+        } catch (\Exception $e) {
+            $this->logger->error($e->getMessage());
+            EventLoop::queue(fn () => exit(1));
+        }
+    }
+}
