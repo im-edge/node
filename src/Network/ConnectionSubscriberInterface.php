06240f2643a6526e252acbf5e36a87484f534314
Thomas Gelf <thomas@gelf.net>
2024-09-06 09:26:19 +0200

    ConnectionSubscriberInterface: require peer type

--- src/Network/ConnectionSubscriberInterface.php
+++ src/Network/ConnectionSubscriberInterface.php
@@ -3,10 +3,11 @@
 namespace IMEdge\Node\Network;
 
 use IMEdge\JsonRpc\JsonRpcConnection;
+use IMEdge\Node\Rpc\RpcPeerType;
 
 interface ConnectionSubscriberInterface
 {
-    public function activateConnection(string $hexUuid, JsonRpcConnection $connection): void;
+    public function activateConnection(string $hexUuid, JsonRpcConnection $connection, RpcPeerType $peerType): void;
 
     public function deactivateConnection(string $hexUuid): void;
 }
