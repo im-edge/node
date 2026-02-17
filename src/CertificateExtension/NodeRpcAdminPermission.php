49a3cdfcb40d3ba685a906054e74f0530df78a2d
Thomas Gelf <thomas@gelf.net>
2026-02-17 16:12:24 +0100

    NodeRpcAdminPermission: use new ignore syntax

--- src/CertificateExtension/NodeRpcAdminPermission.php
+++ src/CertificateExtension/NodeRpcAdminPermission.php
@@ -8,9 +8,10 @@ use Sop\X509\Certificate\Extension\Extension;
 
 class NodeRpcAdminPermission extends Extension
 {
-    // @codingStandardsIgnoreLine PSR2.Methods.MethodDeclaration.Underscore
+    // @phpcs:disable PSR2.Methods.MethodDeclaration.Underscore
     protected function _valueASN1(): Element
     {
         return new ObjectIdentifier('1.3.6.1.4.1.26840.5670.509.1');
     }
+    // @phpcs:enable PSR2.Methods.MethodDeclaration.Underscore
 }
