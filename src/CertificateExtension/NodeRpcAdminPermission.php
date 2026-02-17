e1c59c262db13274d1319e355c349f726871f5eb
Thomas Gelf <thomas@gelf.net>
2026-02-17 15:57:16 +0100

    NodeRpcAdminPermission: use single line ignore

--- src/CertificateExtension/NodeRpcAdminPermission.php
+++ src/CertificateExtension/NodeRpcAdminPermission.php
@@ -8,12 +8,9 @@ use Sop\X509\Certificate\Extension\Extension;
 
 class NodeRpcAdminPermission extends Extension
 {
-    /**
-     * @codingStandardsIgnoreStart -> PSR2.Methods.MethodDeclaration.Underscore
-     */
+    // @codingStandardsIgnoreLine PSR2.Methods.MethodDeclaration.Underscore
     protected function _valueASN1(): Element
     {
-        // @codingStandardsIgnoreEnd
         return new ObjectIdentifier('1.3.6.1.4.1.26840.5670.509.1');
     }
 }
